0fe72a4888f93c784acd622fd0420c36b073efb0
Hanleys <40015515+HanleysFramer@users.noreply.github.com>
2022-11-25 02:31:46 -0600

    Configured

--- MDLReader/GeneralRunner.cpp
+++ MDLReader/GeneralRunner.cpp
@@ -11,7 +11,7 @@ int main()
     MDLReader newFile = MDLReader();
     MDLReader secondFile = MDLReader();
 
-    newFile.openFile( "C:\\Users\\wauke\\Desktop\\baseModel_j3sh.mdl" );
+    newFile.openFile( "C:\\Users\\wauke\\Desktop\\baseModel_jsh.mdl" );
     //MDLReader secondFile = MDLReader();
     //secondFile.openFile("C:\\.GamingRoot");
         
